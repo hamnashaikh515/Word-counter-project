af1f712a1d5f3aff0c20fdb783c62aa83e8d5442
Hamna Shaikh <160252803+hamnashaikh515@users.noreply.github.com>
2024-06-26 07:58:38 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+//This line is called a shebang, it tells the OS to run it with node.js
+
+//import the 'inquirer' module, which is acommand line interface for Node.js
+import inquirer from "inquirer"
+
+//Declare a constant 'answers' and assign is to the result of inquirer.prompt function
+
+const answers: {
+        sentence: string
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+])
+
+const words = answers.sentence.trim().split(" ")
+
+//print the array of words to the console
+console.log(words)
+
+//print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
+
